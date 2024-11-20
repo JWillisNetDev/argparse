602518f7e1ed3b2d2d2e4ace195978705ee07c2b
Joshua Willis <joshua.willis@intellitect.com>
2024-11-19 19:12:40 -0800

    3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use std::{fs::File, io::{BufReader, BufRead}, path::PathBuf};
 use anyhow::{bail};
-use clap::{command, Parser, arg};
+use chrono::{DateTime, FixedOffset};
+use clap::{arg, command, Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "A simple CLI to read a file")]
 struct Cli {
     #[arg(short, long, help = "The path to the file to read")]
-    file: PathBuf
+    file: PathBuf,
+    #[command(subcommand)]
+    command: Command
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Show,
+    Filter(FilterArgs),
+}
+
+// parsearg <File> filter --timestamp 2021-01-01T00:00:00+00:00
+
+#[derive(Args, Debug)]
+struct FilterArgs {
+    #[arg(long, help = "The timestamp to filter by")]
+    timestamp: Option<DateTime<FixedOffset>>,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -28,8 +45,15 @@ fn main() -> anyhow::Result<()> {
             }
         });
 
-    for line in lines {
-        println!("{}", line);
+    match cli.command {
+        Command::Show => {
+            for line in lines {
+                println!("{}", line);
+            }
+        },
+        Command::Filter => {
+            
+        }
     }
 
     Ok(())
