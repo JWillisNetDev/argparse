436e466062e236bd5756a7716107df309f41e589
Joshua Willis <joshua.willis@intellitect.com>
2024-11-19 19:01:39 -0800

    next commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-use std::path::PathBuf;
-
-use clap::{command, Parser};
+use std::{fs::File, io::{BufReader, BufRead}, path::PathBuf};
+use anyhow::{bail};
+use clap::{command, Parser, arg};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "A simple CLI to read a file")]
 struct Cli {
+    #[arg(short, long, help = "The path to the file to read")]
     file: PathBuf
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    println!("{}", cli.file.display());
+    let file = File::open(&cli.file)?;
+    let reader = BufReader::new(file);
+
+    let lines = reader.lines()
+        .filter_map(|line| {
+            if let Ok(line) = line {
+                if line.is_empty() {
+                    None
+                } else {
+                    Some(line)
+                }
+            } else {
+                None
+            }
+        });
+
+    for line in lines {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
